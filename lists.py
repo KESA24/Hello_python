982b8815719638fe2b73458b180d11b21bf627a3
kesa <nekepatri@gmail.com>
2021-01-30 01:42:53 +0300

    python lists

---
+++ lists.py
@@ -0,0 +1,10 @@
+# Python lists
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+print(friends[0])
+print(friends[-1])
+print(friends[-2])
+print(friends[1:])
+print(friends[1:3])
+
+friends[1] = "Kesa"
+print(friends)
