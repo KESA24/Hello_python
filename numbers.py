e22648559579c052ee622724d8f21d058828f5e4
kesa <nekepatri@gmail.com>
2021-01-30 01:01:53 +0300

    Started on numbers

---
+++ numbers.py
@@ -0,0 +1,13 @@
+# Working with python numbers
+print(2)
+print(2+3.4)
+print(2*3.4)
+print(3*(4+5))
+# Modulus gives us remainder after division, ans below =1
+print(10%3)
+
+my_num = 5
+print(my_num)
+
+# To print out a number in a string, you must use the "str" function otw the programme will throw an error
+print(str(my_num) + " is my favorite number")
