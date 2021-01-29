78c69e1e8ea66c7038ac545a6fd977021bcfcb9d
kesa <nekepatri@gmail.com>
2021-01-30 01:25:33 +0300

    built a basic calculator

---
+++ calculator.py
@@ -0,0 +1,9 @@
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+# Returns only whole number
+# result = int(num1) + int(num2)
+
+# float uses both decimals and whole numbers
+result = float(num1) + float(num2)
+print(result)
